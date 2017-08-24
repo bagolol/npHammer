d112ef97c9ad44b0e3973965389e254ab7091625
rocco <rocco.righi@bbc.co.uk>
2017-08-24 17:05:38 +0100

    Filter releases and add comments

--- server.js
+++ server.js
@@ -15,40 +15,9 @@ const isRelevantPackage = filename => {
     return packageNames.indexOf(filename) > -1;
 }
 
-
+// here we filter all modules to get only the ones in our package.json
 getJSONFiles().then(files => files.filter(isRelevantPackage)).
         then(locations => locations.map(location => parseJson(require(location))));
-
-
-
-const parseJson = (json) => {
-    let owner, repoName;
-    const version = json._id.match(/[^@]+$/g);;
-    const regex = new RegExp('com\/(.*?)\.git');
-    [owner, repoName] = json.repository.url.match(regex)[1].split('/');
-    const uri = `${baseURL}${owner}/${repoName}`;
-    getRepo(config(uri)).then(json => analyseRepo(version, json));
-}
-
-const getReleaseDate = (release, json) => {
-    const uri = `${baseURL}${json.full_name}/releases/tags/${release}`;
-    return new Promise ((resolve, reject) => {
-        getRepo(config(uri)).then(result => {
-            resolve(result.published_at);
-        }).catch(err => reject(err))
-    })
-}
-
-const analyseRepo = (release, json) => {
-    getReleaseDate(release, json).then(date => {
-        const days = getReleaseAge(json.pushed_at, date);
-    })
-}
-const getReleaseAge = (current, local) => {
-    current = Date.parse(current);
-    local = Date.parse(local);
-    return (current - local) / 86400000;
-}
 
 const config = uri => ({
     method: 'GET',
@@ -68,5 +37,43 @@ const getRepo = myConfig => {
     })
 }
 
+//we need to make a call to github api to get the repositories' metadata
+const parseJson = (json) => {
+    let owner, repoName;
+    const version = json._id.match(/[^@]+$/g);;
+    const regex = new RegExp('com\/(.*?)\.git');
+    [owner, repoName] = json.repository.url.match(regex)[1].split('/');
+    const uri = `${baseURL}${owner}/${repoName}`;
+    getRepo(config(uri)).then(json => analyseRepo(version, json));
+}
+// we pass the json containing the metadata
+// and the actual release number we've installed
+// this way we can calculate how old our version is
+const analyseRepo = (release, json) => {
+    getReleaseDate(release, json).then(date => {
+        const days = getReleaseAge(json.pushed_at, date);
+    })
+}
+// we make a call to the releases endpoint and filter the
+// relevant version. this is because the release number are
+// not consistent
+const getReleaseDate = (version, json) => {
+    const uri = `${baseURL}${json.full_name}/releases`;
+    return new Promise ((resolve, reject) => {
+        getRepo(config(uri)).then(releases => {
+            const releaseDate = getRelease(releases, version)[0].published_at;
+            resolve(releaseDate);
+        }).catch(err => reject(err))
+    })
+}
 
+const getRelease = (rels, version) => rels.filter(rel => rel.tag_name.includes(version));
+
+// here we calculate how many days have passed
+// since our version was released compared to HEAD
+const getReleaseAge = (current, local) => {
+    current = Date.parse(current);
+    local = Date.parse(local);
+    return (current - local) / 86400000;
+}
 
